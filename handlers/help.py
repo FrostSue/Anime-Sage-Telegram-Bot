038769283d6ffea437b57f4107eeb3e37fbcd73a
FrostSue <emirhan_58600@hotmail.com>
2025-12-08 05:09:47 +0300

    feat: Make /help command dynamic showing extended options for admins

--- handlers/help.py
+++ handlers/help.py
@@ -1,11 +1,18 @@
 from pyrogram import Client
 from pyrogram.types import Message
 from utils.languages import t
-from utils.auth import check_user_registration
+from utils.auth import check_user_registration, is_admin
 
 async def help_handler(c: Client, m: Message):
     is_valid, lang = await check_user_registration(c, m)
     if not is_valid:
         return
 
-    await m.reply_text(t("help_text", lang))
+    user_is_admin = await is_admin(m.from_user.id)
+    
+    if user_is_admin:
+        help_key = "help_text_admin"
+    else:
+        help_key = "help_text"
+
+    await m.reply_text(t(help_key, lang))
