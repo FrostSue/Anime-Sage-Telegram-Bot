490f30bce44a27e9d529e5a9ece706723a73dcaa
FrostSue <emirhan_58600@hotmail.com>
2025-12-08 03:13:50 +0300

    refactor(help): ensure user is registered before showing help

--- handlers/help.py
+++ handlers/help.py
@@ -1,8 +1,11 @@
 from pyrogram import Client
 from pyrogram.types import Message
-from database.db import get_group_lang
 from utils.languages import t
+from utils.auth import check_user_registration
 
 async def help_handler(c: Client, m: Message):
-    lang = await get_group_lang(m.chat.id)
+    is_valid, lang = await check_user_registration(c, m)
+    if not is_valid:
+        return
+
     await m.reply_text(t("help_text", lang))
