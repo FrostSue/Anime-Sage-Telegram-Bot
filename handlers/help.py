c8122547d8ad88d1314cbdbb10cacc6db6943dc0
FrostSue <emirhan_58600@hotmail.com>
2025-12-08 05:15:25 +0300

    refactor: Simplify help handler removing redundant admin checks

--- handlers/help.py
+++ handlers/help.py
@@ -1,18 +1,11 @@
 from pyrogram import Client
 from pyrogram.types import Message
 from utils.languages import t
-from utils.auth import check_user_registration, is_admin
+from utils.auth import check_user_registration
 
 async def help_handler(c: Client, m: Message):
     is_valid, lang = await check_user_registration(c, m)
     if not is_valid:
         return
 
-    user_is_admin = await is_admin(m.from_user.id)
-    
-    if user_is_admin:
-        help_key = "help_text_admin"
-    else:
-        help_key = "help_text"
-
-    await m.reply_text(t(help_key, lang))
+    await m.reply_text(t("help_text", lang))
